30afea79f5e558f435ef4548da67ade94b061cd1
ckiesl <c.kiesl@googlemail.com>
2017-03-02 00:50:21 +0100

    Inserted mongodb support
    make_datasets now saves the entire content of the messages in the active
    mongodb database.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -7,18 +7,28 @@ import re
 'def'
 import codecs
 import functools
+from pymongo import MongoClient
+import datetime
+import time
+
+def  getMessage(threadId,sender, text, date):
+    return {"threadId":threadId, "sender":sender, "text":text, "date":date}
+
+def getThread(id,participants):
+    return {"id":id, "participants":participants}
+
+
 
 
 def main(input_filepath, output_filepath):
+    client = MongoClient()
+    db = client['facebook']
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-    print(input_filepath)
-    print(output_filepath)
     messages = codecs.open(input_filepath+'\\messages.htm', 'r', 'UTF-8')
-    print(type(messages))
         #messages = open(input_filepath+'\\messages.htm','r')
     soup = BeautifulSoup(messages, 'html.parser')
     divs = soup.find_all('div')
@@ -26,10 +36,33 @@ def main(input_filepath, output_filepath):
     content = divs[1]
     threads = content.find_all('div', class_ = 'thread')
     members = map(lambda x: re.findall('[0-9]*@facebook.com', str(x)),threads)
+    members = map(lambda x: list(set(x)), members)
     messageAll = map(lambda x: zip(x.find_all('span', class_='user'), x.find_all('span',class_='meta'), x.find_all('p')), threads)
     #MessageAll now contains a list of threads, where each thread again contains a list of message in the form (user,meta,message)
     #members[i] contains the members of thread i
     #The next step is saving this information in a database
+    
+    #Mongodb
+    messageCollection = db.messages
+    i=0
+    for (thread,participants) in zip(messageAll,members):
+        print(participants)
+        m = []
+        for x in thread:
+            text = str(x[2])[3:-4]
+            user = str(x[0])[19:-7]
+            meta = str(x[1])[19:-14].split(",")[1]
+            meta = meta.replace("Januar","1.").replace("Februar","2.").replace("März","3.").replace("April","4.").replace("Mai","5.").replace("Juni","6.").replace("Juli","7.").replace("August","8.").replace("September","9.").replace("Oktober","10.").replace("November","11.").replace("Dezember","12.")
+            meta = meta.replace(" ","").replace("um"," ")
+            new_date = time.strptime(meta,'%d.%m.%Y %H:%M')
+            m.append({"user":user,"meta":meta,"text":text})
+        thread = {"_id":i, "members": participants, "messages":m}
+        i = i+1
+        if(i==2):
+            return "1"
+        messageCollection.insert_one(thread)
+    
+    
 
 if __name__ == '__main__':
     os.chdir(os.getcwd())
